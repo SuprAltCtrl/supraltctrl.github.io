c05c458661454be977c0e03b59e5083ee5792742
SuprAltCtrl <pe.gmz.ma@gmail.com>
2021-03-18 19:22:33 +0100

    update site

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,19 @@ var store = [{
         "excerpt":"Enunciado Vamos a crear una función matemática que debe comprobar el formato de la fórmula que se pasa por parámetro, comprobando que todos los paréntesis que tiene la fórmula están nivelados (es decir, están por pares). Devolverá true si están por parejas y false en caso contrario (hay más paréntesis...","categories": [],
         "tags": ["Programación","Java","Stack"],
         "url": "http://0.0.0.0:4000/balanceo-parentesis-java/",
-        "teaser":"http://0.0.0.0:4000/assets/images/java/java.png"}]
+        "teaser":null},{
+        "title": "Java – Análisis de un texto",
+        "excerpt":"Enunciado Tenemos un texto muy largo (aquí puedes coger el que quieras, siempre que sea muy largo), del que deseamos comprobar cuántas apariciones hay de cada uno de las letras que tenemos en español. Una vez analizado el texto, mostrar por pantalla la letra, el porcentaje de veces que aparece,...","categories": [],
+        "tags": ["Java","Arrays","Programacion"],
+        "url": "http://0.0.0.0:4000/analisis-documento-java/",
+        "teaser":null},{
+        "title": "Java - Adivina el número",
+        "excerpt":"Enunciado Este minijuego consiste en adivinar el número que genera aleatoriamente entre un rango (mínimo - máximo) que se le pasa por teclado, también le pasamos el número de intentos para ver si somos capaces de adivinarlo en menos intentos del que hemos puesto. Solución En el código se han...","categories": [],
+        "tags": ["Programación","Java","Juego"],
+        "url": "http://0.0.0.0:4000/adivina-el-numero/",
+        "teaser":null},{
+        "title": "HackTheBox - Máquina Lame",
+        "excerpt":"Enumeración Lanzamos un escaneo para ver los puertos que tiene abiertos y después lanzamos para obtener versiones y scripts básicos de nmap. nmap -sS --min-rate 5000 -p- --open -T5 -v -n -Pn -oG allPorts 10.10.10.3 nmap -sV -sC -p21,22,139,445,3632 10.10.10.3 -oN targeted Y obtenemos el siguiente resultado: 21/tcp open ftp...","categories": [],
+        "tags": ["Htb","Pentesting","Metasploit"],
+        "url": "http://0.0.0.0:4000/htb-lame/",
+        "teaser":"http://0.0.0.0:4000/assets/images/htb/lame/lame.png"}]
